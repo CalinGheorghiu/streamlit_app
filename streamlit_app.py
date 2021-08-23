f82388e8e3ddc6d5c28e102bd98464e4ce508bce
CalinGheorghiu <58596544+CalinGheorghiu@users.noreply.github.com>
2021-08-23 12:47:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,31 +14,33 @@ login=st.container()
 username=st.text_input("Username",value="")
 password=st.text_input("Password",value="")
 with login:
-    username
-    password
+    st.write(username)
+    st.write(password)
     submit=st.button("Log in")
 
-with st.form(key='template-form'):
-        instance_name=st.text_input("Instance Name")
-        template_source=st.selectbox("Template",[1,2,3])
-        start_date=st.date_input("Start Date")
-        stop_date=st.date_input("Stop Date")
-        pace=st.selectbox("Pace",['Daily','Weekly','Monthly'])
-        operator_id=st.multiselect("OperatorID",list(df['OperatorID']))
-        all_operators=st.checkbox("All Operators")
-        leather_type=st.multiselect("Leather Type",list(df['Leather type']))
-        all_leather_types=st.checkbox("All Leather Types")
-        output=st.multiselect("Output",["Scan time"," Recuts", "Yield"])
-        submit=st.form_submit_button("Submit")
 if submit:
-    my_table = st.table(instance_df)
-    st.write(instance_name)
-    df1=st.session_state['df'].copy()
-    if all_operators is False:
-        df1=df1[df1['OperatorID'].isin(operator_id)]
-    if all_operators is False:
-        df1=df1[df1['Leather type'].isin(leather_type)]
-    st.write(df1[['OperatorID','Leather type','scanned area','time','date']])
-    st.session_state['df']=st.session_state['df'].append(df1)
-    
+    if username=="admin" and password=="admin":
+        with st.form(key='template-form'):
+                instance_name=st.text_input("Instance Name")
+                template_source=st.selectbox("Template",[1,2,3])
+                start_date=st.date_input("Start Date")
+                stop_date=st.date_input("Stop Date")
+                pace=st.selectbox("Pace",['Daily','Weekly','Monthly'])
+                operator_id=st.multiselect("OperatorID",list(df['OperatorID']))
+                all_operators=st.checkbox("All Operators")
+                leather_type=st.multiselect("Leather Type",list(df['Leather type']))
+                all_leather_types=st.checkbox("All Leather Types")
+                output=st.multiselect("Output",["Scan time"," Recuts", "Yield"])
+                submit=st.form_submit_button("Submit")
+        if submit:
+            my_table = st.table(instance_df)
+            st.write(instance_name)
+            df1=st.session_state['df'].copy()
+            if all_operators is False:
+                df1=df1[df1['OperatorID'].isin(operator_id)]
+            if all_operators is False:
+                df1=df1[df1['Leather type'].isin(leather_type)]
+            st.write(df1[['OperatorID','Leather type','scanned area','time','date']])
+            st.session_state['df']=st.session_state['df'].append(df1)
+        
 
