d4f92ec74bc14967a7e7fbe41fe1b3a3a94181ae
CalinGheorghiu <58596544+CalinGheorghiu@users.noreply.github.com>
2021-08-23 12:28:51 +0300

    files

---
+++ stream_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+from datetime import date
+st.session_state['df'] = pd.read_csv("Scan_data.csv")
+instance_df=pd.DataFrame(
+    columns=['Template','Instance Name']
+)
+instance_dict={}
+k=0
+col1,col2=st.columns(2)
+templates=[]
+names=[]
+with st.form(key='template-form'):
+        instance_name=st.text_input("Instance Name")
+        template_source=st.selectbox("Template",[1,2,3])
+        start_date=st.date_input("Start Date")
+        stop_date=st.date_input("Stop Date")
+        pace=st.selectbox("Pace",['Daily','Weekly','Monthly'])
+        operator_id=st.multiselect("OperatorID",list(df['OperatorID']))
+        all_operators=st.checkbox("All Operators")
+        leather_type=st.multiselect("Leather Type",list(df['Leather type']))
+        all_leather_types=st.checkbox("All Leather Types")
+        output=st.multiselect("Output",["Scan time"," Recuts", "Yield"])
+        submit=st.form_submit_button("Submit")
+if submit:
+    my_table = st.table(instance_df)
+    st.write(instance_name)
+    df1=st.session_state['df'].copy()
+    if all_operators is False:
+        df1=df1[df1['OperatorID'].isin(operator_id)]
+    if all_operators is False:
+        df1=df1[df1['Leather type'].isin(leather_type)]
+    st.write(df1[['OperatorID','Leather type','scanned area','time','date']])
+    st.session_state['df']=st.session_state['df'].append(df1)
+    
+
